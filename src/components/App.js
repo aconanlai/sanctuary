412985d27cf85a90482fd9011285c0f5cc9ba959
Conan <aconanlai@gmail.com>
2017-08-13 12:21:29 -0400

    sections

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,12 @@ import React from 'react';
 import Nav from './Nav/Nav';
 import Block from './blocks/Block';
 import Home from './blocks/Home/Home';
+import About from './blocks/About/About';
+import Map from './blocks/Map/Map';
+import Stories from './blocks/Stories/Stories';
+import Artist from './blocks/Artist/Artist';
+import Credits from './blocks/Credits/Credits';
+
 import './App.css';
 
 const App = () => (
@@ -9,6 +15,21 @@ const App = () => (
     <Nav />
     <Block>
       <Home />
+    </Block>
+    <Block>
+      <About />
+    </Block>
+    <Block>
+      <Map />
+    </Block>
+    <Block>
+      <Stories />
+    </Block>
+    <Block>
+      <Artist />
+    </Block>
+    <Block>
+      <Credits />
     </Block>
   </div>
 );
