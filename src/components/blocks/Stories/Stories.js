d98ff7e24f7dd0169b0450eaa281efd6d056e3a1
Conan <aconanlai@gmail.com>
2017-08-17 14:17:53 -0400

    stories + styling

--- src/components/blocks/Stories/Stories.js
+++ src/components/blocks/Stories/Stories.js
@@ -4,9 +4,29 @@ import './Stories.css';
 const Stories = () => (
   <div id="stories">
     <h1>Stories</h1>
-    <p>
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-    </p>
+    <div className="storiesList">
+      <div className="story">
+        <div className="storySquare" />
+        <span className="storyName">Story 1</span>
+      </div>
+      <div className="story">
+        <div className="storySquare" />
+        <span className="storyName">Story 2</span>
+      </div>
+      <div className="story">
+        <div className="storySquare" />
+        <span className="storyName">Story 3</span>
+      </div>
+      <div className="lineBreak" />
+      <div className="story">
+        <div className="storySquare" />
+        <span className="storyName">Story 4</span>
+      </div>
+      <div className="story">
+        <div className="storySquare" />
+        <span className="storyName">Story 5</span>
+      </div>
+    </div>
   </div>
 );
 
